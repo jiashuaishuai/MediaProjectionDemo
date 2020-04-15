4f7ced403f098049ec75daa7a9a99d19c168a738
jss <shuaishuai.jia@raykite.com>
2020-04-15 20:21:12 +0800

    Initial commit

---
+++ app/src/main/java/com/ts4/mediaprojectiondemo/MainActivity.java
@@ -0,0 +1,230 @@
+package com.ts4.mediaprojectiondemo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+    private int result = 0;
+    private Intent intent = null;
+    private int REQUEST_MEDIA_PROJECTION = 1;
+    private MediaProjectionManager mMediaProjectionManager;
+    private static final int STORAGE_REQUEST_CODE = 102;
+
+    private int type = ScreenshotsService.CAPTURE;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_REQUEST_CODE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_REQUEST_CODE);
+        }
+
+
+        RadioGroup rg = findViewById(R.id.rg);
+        RadioButton rbCapture = findViewById(R.id.rb_capture);
+        RadioButton rbRecord = findViewById(R.id.rb_record);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_capture:
+                        type = ScreenshotsService.CAPTURE;
+                        break;
+                    case R.id.rb_record:
+                        type = ScreenshotsService.START_RECORD_VIDEO;
+                        break;
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                finish();
+            }
+        }
+    }
+
+
+    /**
+     * 录屏
+     *
+     * @param view
+     */
+    public void recordVideo(View view) {
+
+        sendMessage(ScreenshotsService.START_RECORD_VIDEO);
+
+    }
+
+    public void stopRecordVideo(View view) {
+        sendMessage(ScreenshotsService.STOP_RECORD_VIDEO);
+    }
+
+
+    /**
+     * 截图
+     *
+     * @param view
+     */
+    public void screenshot(View view) {
+        sendMessage(ScreenshotsService.CAPTURE);
+
+    }
+
+    /**
+     * 启动服务器，bindingService
+     *
+     * @param view
+     */
+    public void startService(View view) {
+        mMediaProjectionManager = (MediaProjectionManager) getApplication().getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        startActivityForResult(mMediaProjectionManager.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION);
+    }
+
+
+    /**
+     * 初始化服务
+     *
+     * @param v
+     */
+    public void initService(View v) {
+        sendMessage(ScreenshotsService.INIT_SERVICE, type);
+    }
+
+    /**
+     * 停止服务
+     *
+     * @param view
+     */
+    public void stopService(View view) {
+        sendMessage(ScreenshotsService.STOP_SERVICE);
+    }
+
+    public void sendMessage(int code, Object content) {
+
+        if (isConn) {
+            Message msgFromClient = Message.obtain(null, code);
+
+            if (content != null) {
+                if (content instanceof Integer) {
+                    msgFromClient.arg1 = (int) content;
+                } else if (content instanceof String) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("content", (String) content);
+                    msgFromClient.setData(bundle);
+                }
+            }
+
+            msgFromClient.replyTo = mMessenger;
+            try {
+                mService.send(msgFromClient);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(int code) {
+        sendMessage(code, null);
+    }
+
+    private Messenger mService;
+    private boolean isConn;
+    private Messenger mMessenger = new Messenger(new Handler() {
+        @Override
+        public void handleMessage(Message msgFromServer) {
+            switch (msgFromServer.what) {
+                case ScreenshotsService.CAPTURE_SUCCESS:
+                    Bundle bundle = msgFromServer.getData();
+                    String path = bundle.getString("path");
+                    Log.d(TAG, "path:    " + path);
+                    break;
+                case ScreenshotsService.RECORD_TIMER:
+                    int time = msgFromServer.arg1;
+                    Log.d(TAG, "time:  " + time);
+                    break;
+                case ScreenshotsService.RECORD_VIDEO_SUCCESS:
+                    Bundle videoBundle = msgFromServer.getData();
+                    String videoPath = videoBundle.getString("VideoPath");
+                    Log.d(TAG, "VideoPath:    " + videoPath);
+                    break;
+            }
+            super.handleMessage(msgFromServer);
+        }
+    });
+
+    private ServiceConnection mConn = new ServiceConnection() {
+
+        //IBinder 对象，需要Bundle包装，传给Unity页面，和service进行通信
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = new Messenger(service);
+            isConn = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+            isConn = false;
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            } else if (data != null && resultCode != 0) {
+                result = resultCode;
+                intent = data;
+                Intent intent = new Intent(getApplicationContext(), ScreenshotsService.class);
+                intent.putExtra("code", resultCode);
+                intent.putExtra("data", data);
+                bindService(intent, mConn, Context.BIND_AUTO_CREATE);
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/ts4/mediaprojectiondemo/ScreenshotsService.java
@@ -0,0 +1,424 @@
+package com.ts4.mediaprojectiondemo;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+
+public class ScreenshotsService extends Service {
+    public int mResultCode = 0;
+    public Intent mResultData = null;
+    public MediaProjectionManager mMediaProjectionManager;
+    private MediaProjection mMediaProjection;
+    private VirtualDisplay mVirtualDisplay;
+    private int windowWidth = 0;
+    private int windowHeight = 0;
+
+
+    public static final int INIT_SERVICE = 0x111;
+    public static final int STOP_SERVICE = 0x112;
+
+
+    public static final int CAPTURE = 0x110;
+    public static final int CAPTURE_SUCCESS = 0x1113;
+
+    public static final int START_RECORD_VIDEO = 0x120;
+    public static final int STOP_RECORD_VIDEO = 0x124;
+    public static final int RECORD_VIDEO_SUCCESS = 0x123;
+
+    public static final int RECORD_TIMER = 0x130;
+
+    private Messenger mMessenger;
+    private String TAG = "ScreenshotsService";
+    private DisplayMetrics metrics;
+    private int mScreenDensity;
+    private ImageReader mImageReader;
+    private MediaRecorder mediaRecorder;
+
+    private SimpleDateFormat dateFormat;
+    private String strDate;
+    private String rootDirectory;
+    private String nameImage;
+    public String nameVideo;
+
+    public boolean recordRun;
+
+    private int type;
+
+    private int recordTotalSize = 60 * 3;
+    private int recordSize;//录制时长，单位秒
+
+    static class MessageHandler extends Handler {
+        ScreenshotsService service;
+
+        public MessageHandler(ScreenshotsService s) {
+            service = s;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what) {
+                case CAPTURE:
+                    service.startCapture(msg);
+                    break;
+                case START_RECORD_VIDEO:
+                    service.startRecord(msg);
+                    break;
+                case STOP_RECORD_VIDEO:
+                    service.stopRecord(msg);
+                    break;
+                case INIT_SERVICE:
+                    service.initVirtualDisplay(msg.arg1);
+                    break;
+                case STOP_SERVICE:
+                    service.stopCaptureService();
+                    break;
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        dateFormat = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss");
+        strDate = dateFormat.format(new java.util.Date());
+        rootDirectory = Environment.getExternalStorageDirectory().getPath() + "/Pictures/";
+
+        mMessenger = new Messenger(new MessageHandler(this));
+        mMediaProjectionManager = (MediaProjectionManager) getApplication().getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        WindowManager mWindowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
+        Point realSize = new Point();
+        mWindowManager.getDefaultDisplay().getRealSize(realSize);
+        windowWidth = realSize.x;
+        windowHeight = realSize.y;
+        metrics = new DisplayMetrics();
+        mWindowManager.getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        mResultCode = intent.getIntExtra("code", -1);
+        mResultData = intent.getParcelableExtra("data");
+        return mMessenger.getBinder();
+    }
+
+
+    public void stopCaptureService() {
+        if (mImageReader != null) {
+            mImageReader.close();
+            mImageReader = null;
+        }
+        if (mediaRecorder != null) {
+            try {
+                mediaRecorder.setOnErrorListener(null);
+                mediaRecorder.setOnInfoListener(null);
+                mediaRecorder.setPreviewDisplay(null);
+                mediaRecorder.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                mediaRecorder.reset();
+                mediaRecorder.release();
+                mediaRecorder = null;
+            }
+
+
+        }
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+            mVirtualDisplay = null;
+        }
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+        stopForeground(true);
+
+    }
+
+
+    /**
+     * 初始化截屏服务   type：服务类型，截屏CAPTURE，录屏START_RECORD_VIDEO
+     */
+    public void initVirtualDisplay(int type) {
+        this.type = type;
+        if (mMediaProjection == null) {
+            setUpMediaProjection();
+        }
+        if (type == CAPTURE) {
+            if (mImageReader == null || mVirtualDisplay == null)//截取屏幕需要先初始化  virtualDisplay
+                virtualDisplay();
+        }
+    }
+
+    private void setUpMediaProjection() {
+        createNotificationChannel();//构建通知栏，适配api 29,小于29可以不用，
+        mMediaProjection = mMediaProjectionManager.getMediaProjection(mResultCode, mResultData);
+        Log.d(TAG, "mMediaProjection 初始化成功");
+    }
+
+    private void virtualDisplay() {
+        mVirtualDisplay = mMediaProjection.createVirtualDisplay("screen-mirror",
+                windowWidth, windowHeight, mScreenDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                getSurface(), null, null);
+    }
+
+
+    private Surface getSurface() {
+        if (type == CAPTURE) {
+            initImageReader();
+            return mImageReader.getSurface();
+        } else {
+            initMediaRecorder();
+            return mediaRecorder.getSurface();
+        }
+    }
+
+    private void initImageReader() {
+        mImageReader = ImageReader.newInstance(windowWidth, windowHeight, 0x1, 2); //ImageFormat.RGB_565
+    }
+
+
+    private void initMediaRecorder() {
+        mediaRecorder = new MediaRecorder();
+//        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+
+        strDate = dateFormat.format(new java.util.Date());
+        nameVideo = rootDirectory + strDate + ".mp4";
+        mediaRecorder.setOutputFile(nameVideo);
+        mediaRecorder.setVideoSize(windowWidth, windowHeight);
+
+
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+//        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
+        mediaRecorder.setVideoEncodingBitRate(5 * windowWidth * windowHeight);
+        mediaRecorder.setVideoFrameRate(30);
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 停止录制
+     *
+     * @param msg
+     */
+    public void stopRecord(Message msg) {
+        try {
+            if (recordRun) {
+                if (mediaRecorder != null) {
+                    mediaRecorder.setOnErrorListener(null);
+                    mediaRecorder.setOnInfoListener(null);
+                    mediaRecorder.setPreviewDisplay(null);
+                    mediaRecorder.stop();
+                }
+                if (mVirtualDisplay != null) {
+                    mVirtualDisplay.release();
+                }
+                Log.d(TAG, "停止录制");
+                //录制完成通知客户端
+                Message toClient = Message.obtain(msg);
+                toClient.what = RECORD_VIDEO_SUCCESS;
+                Bundle bundle = new Bundle();
+                bundle.putString("VideoPath", nameVideo);
+                toClient.setData(bundle);
+                msg.replyTo.send(toClient);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mediaRecorder != null) {
+                mediaRecorder.reset();
+                mediaRecorder.release();
+                mediaRecorder = null;
+            }
+            mVirtualDisplay = null;
+            recordRun = false;
+        }
+    }
+
+    /**
+     * 开始录制
+     * mediaRecorder  每次录制完毕后再次录制需要重新初始化所有参数，，暂时无解
+     *
+     * @param msg
+     */
+
+    public void startRecord(Message msg) {
+        if (!recordRun) {
+            virtualDisplay();
+            mediaRecorder.start();
+            recordRun = true;
+            Log.d(TAG, "开始录制");
+            recordSize = 0;
+            recordTimer(msg);//启动计时器
+        } else {
+            Log.d(TAG, "录制进行中");
+        }
+    }
+
+    /**
+     * 计时器
+     *
+     * @param msg
+     */
+    private void recordTimer(final Message msg) {
+        final Message toClient = Message.obtain(msg);
+        toClient.what = RECORD_TIMER;
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (recordRun) {
+                    if (recordSize >= recordTotalSize)//录制满时长则退出
+                    {
+                        stopRecord(msg);//停止录制
+                        return;
+                    }
+                    toClient.arg1 = recordSize;
+//                        msg.replyTo.send(toClient);
+                    Log.d(TAG, "录制中 time：" + recordSize + "总时长：" + recordTotalSize);
+                    recordSize++;
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+        handler.post(runnable);
+    }
+
+
+    private void startCapture(Message msg) {
+
+        //client message
+        Message msgToClient = Message.obtain(msg);
+        Bundle bundle = new Bundle();
+        msgToClient.what = CAPTURE_SUCCESS;
+        strDate = dateFormat.format(new java.util.Date());
+        nameImage = rootDirectory + strDate + ".png";
+        bundle.putString("path", nameImage);
+        msgToClient.setData(bundle);
+        //捕获bitmap
+        Image image = mImageReader.acquireLatestImage();
+        int width = image.getWidth();
+        int height = image.getHeight();
+        final Image.Plane[] planes = image.getPlanes();
+        final ByteBuffer buffer = planes[0].getBuffer();
+        int pixelStride = planes[0].getPixelStride();
+        int rowStride = planes[0].getRowStride();
+        int rowPadding = rowStride - pixelStride * width;
+        Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(buffer);
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height);
+        image.close();
+        Log.i(TAG, "捕获图像数据");
+
+        if (bitmap != null) {
+            try {
+                File fileImage = new File(nameImage);
+                if (!fileImage.exists()) {
+                    fileImage.createNewFile();
+                    Log.i(TAG, "创建文件");
+                }
+                FileOutputStream out = new FileOutputStream(fileImage);
+                if (out != null) {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    out.flush();
+                    out.close();
+
+                    //send message 通知 client
+                    msg.replyTo.send(msgToClient);
+                    //通知更新图库
+                    Intent media = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    Uri contentUri = Uri.fromFile(fileImage);
+                    media.setData(contentUri);
+                    this.sendBroadcast(media);
+                    Log.i(TAG, "保存图片");
+
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } finally {
+                bitmap.recycle();
+            }
+        }
+
+    }
+
+    private void createNotificationChannel() {
+        Notification.Builder builder = new Notification.Builder(getApplicationContext()); //获取一个Notification构造器
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)) // 设置下拉列表中的图标(大图标)
+                //.setContentTitle("SMI InstantView") // 设置下拉列表里的标题
+                .setSmallIcon(R.mipmap.ic_launcher) // 设置状态栏内的小图标
+                .setContentText("is running......") // 设置上下文内容
+                .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+
+        /*以下是对Android 8.0的适配*/
+        //普通notification适配
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId("notification_id");
+        }
+        //前台服务notification适配
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel("notification_id", "notification_name", NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Notification notification = builder.build(); // 获取构建好的Notification
+        startForeground(110, notification);
+
+    }
+
+}
